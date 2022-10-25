4743a5d44a282778595845d29770b4a5c3da7fa7
DESKTOP-UAD63F6\Eva <evaradu@hotmail.fr>
2022-10-25 12:42:04 +0200

    correcting some bugs

--- convert2mp3.py
+++ convert2mp3.py
@@ -1,22 +1,46 @@
 from __future__ import unicode_literals
-import youtube_dl
+import os, shutil, subprocess
+from turtle import color
+from termcolor import colored
+import colorama
+from colorama import Fore
+from pathlib import Path
 
-print(" ---------- CONVERT TO MP3 ----------")
-link = input ("Insert the URL : ")
+path = str(Path(__file__).parent.absolute())
 
-video_info = youtube_dl.YoutubeDL().extract_info(url = link,download=False)
-filename = f"{video_info['title']}.mp3"
-ydl_opts = {
-    'format': 'bestaudio/best',
-    'postprocessors': [{
-        'key': 'FFmpegExtractAudio',
-        'preferredcodec': 'mp3',
-        'preferredquality': '720',
-    }],
-     'outtmpl':"downloads/"+filename,
 
-}
+print("| -------------------------------------------- CONVERT TO MP3 -------------------------------------------- ")
+print("|   Insert the URLs :                  ")
+print("|                                      ")
 
-with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-    ydl.download([link])
+
+# GETTING THE SONGS LINKS 
+
+links = []
+link = input ("| >> ")
+while(link!=""):
+    if("&" in link):
+        id = link.index("&")
+        link = link[:id]  # removing the wrong parts of the link ("&index=..." and "&list=...")
+    links.append(link)
+    link = input ("| >> ")
+   
+
+
+# DOWNLOADING THE SONGS 
+
+for l in links: 
+    print("| --------------------------------------------------------------------------------------------------------")
+    title = (subprocess.check_output("youtube-dl --get-title "+str(l)).decode("latin-1")[:-1])
+    colorama.init()
+    print(Fore.BLUE + "| Downloading " + title + " ... ")
+    os.system('youtube-dl -q --extract-audio --audio-format mp3 -o "%(title)s.%(ext)s" ' + str(l))   # -o to get a proper name of file
+    print(Fore.GREEN + "| " + title + " downloaded !")
+
+
+# MOVING THE MUSICS IN THE DOWNLOAD FOLDER
+
+musics = [f for f in os.listdir(path) if f.endswith('.mp3')]
+for m in musics:
+    shutil.move( path + "/" + str(m), path + "/downloads/" + str(m) )
 
